c4873ae1fa90ca756950defa1f7c4a883797369f
Daredevil <j.manuel.nunez@me.com>
2022-06-17 14:52:36 -0400

    Update app.component.spec.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,7 +16,7 @@ describe('AppComponent', () => {
   it(`should have as title 'Test-santander'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('formulario-angular7-bootstrap4');
+    expect(app.title).toEqual('formulario-santander');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
