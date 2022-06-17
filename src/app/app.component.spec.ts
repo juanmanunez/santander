873d1816c6c7aef683b04b8a39fb9f48eab29d5e
Daredevil <j.manuel.nunez@me.com>
2022-06-17 14:51:56 -0400

    Update app.component.spec.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,7 +13,7 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'formulario-angular7-bootstrap4'`, async(() => {
+  it(`should have as title 'Test-santander'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('formulario-angular7-bootstrap4');
